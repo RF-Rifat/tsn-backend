f71827d0fe5f691bc354ce868a87c70f7d90dbc8
rfrifat <rfrifat6344@gmail.com>
2025-02-02 21:52:32 +0600

    feat:cors url added

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,74 @@
+// import cookieParser from 'cookie-parser';
+// import cors from 'cors';
+// import express, { Application } from 'express';
+// import GlobalErrorHandler from './app/middlewares/GlobalErrorHanlder';
+// import router from './app/routes';
+// import handleNotFoundApi from './errors/handleNotFound';
+// const app: Application = express();
+
+// app.use(
+//   cors({
+//     origin: ['http://localhost:3000', 'https://tsneduglownetwork.com'],
+//     credentials: true,
+//   }),
+// );
+
+// app.use(cookieParser());
+
+// app.get('/', (req, res) => {
+//   res.send('Server Working successfully');
+// });
+// //parser
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// // route
+// app.use('/api/v1', router);
+// // Global Error handler
+// app.use(GlobalErrorHandler);
+
+// // handle not found api/ route
+// app.use(handleNotFoundApi);
+
+// export default app;
+
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
+// import cors from 'cors';
 import express, { Application } from 'express';
 import GlobalErrorHandler from './app/middlewares/GlobalErrorHanlder';
 import router from './app/routes';
 import handleNotFoundApi from './errors/handleNotFound';
+
 const app: Application = express();
 
-app.use(
-  cors({
-    origin: ['http://localhost:3000', 'https://tsneduglownetwork.com/'],
-    credentials: true,
-  }),
-);
+// CORS configuration
+// app.use(
+//   cors({
+//     origin: ['http://localhost:3000', 'https://tsneduglownetwork.com'],
+//     credentials: true,
+//   }),
+// );
+
+// Handle OPTIONS requests
+// app.options('*', cors());
 
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
-  res.send('Server Working successfully');
+  res.send('TSN Server Working successfully');
 });
-//parser
+
+// Parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// route
+// Routes
 app.use('/api/v1', router);
+
 // Global Error handler
 app.use(GlobalErrorHandler);
 
-// handle not found api/ route
+// Handle not found API/route
 app.use(handleNotFoundApi);
 
 export default app;
